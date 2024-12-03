3c4a2796e3cf8e9547a57b3b62ac037523144539
Luan Oliveira de Brito Nunes <97767459+LuanOliveiraBrito@users.noreply.github.com>
2024-12-03 11:31:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use('/api', vehicleRoutes);
 app.use('/api', driverRoutes);
 app.use('/api', historyRoutes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const frontendPath = path.join(__dirname, 'dist');
+app.use(express.static(frontendPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server running on port ${PORT}`);
